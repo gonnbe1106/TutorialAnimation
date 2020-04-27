d78610136f87b6ca4085ebcc9df179e3d1c5a36b
gonnbe1106 <gonnbe1106@yahoo.co.jp>
2020-04-27 22:10:57 +0900

    Initial Commit

---
+++ TutorialAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TutorialAnimation
+//
+//  Created by 五十嵐伸雄 on 2020/04/27.
+//  Copyright © 2020 五十嵐伸雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
